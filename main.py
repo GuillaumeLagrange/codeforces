563943164c9d6b488a47de378bc07c8d69c51e2a
Guillaume Lagrange <guillaumelag@theodo.fr>
2023-04-25 16:53:03 +0200

    feat: time limit exceeded on test 8

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import sys
 input = sys.stdin.readline
 
 
+EXPECTED_4 = "0 1 2 3 4 5 6 7 8 8 7 6 5 4 3 2 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 21 20 19 18 17 16 15 14 13 12 11 10 9 8 7 6 5 4 3 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 18 17 16 15 14 13 12 11 10 9 8 7 6 5 4 3 4 4 5 6 5 6 7 8"
+
+
 def inp():
     """For taking integer inputs."""
     return (int(input()))
@@ -28,9 +31,6 @@ def main():
     n = inp()
     shortcuts = inlt()
 
-    # print(n)
-    # print(shortcuts)
-
     paths = [0]
     # Neighbor[i] = nodes that can reach i with a cost of 1
     graph = [[1]]
@@ -46,20 +46,29 @@ def main():
         if i not in graph[shortcuts[i] - 1]:
             graph[shortcuts[i] - 1].append(i)
 
-    for i in range(n):
-        newPath = paths[i]
+    while True:
+        newIteration = paths.copy()
 
-        # print('node', i)
-        for neighbor in graph[i]:
-            # print('neighbor', neighbor)
-            # print(paths[neighbor])
-            newPath = min(newPath, paths[neighbor] + 1)
+        for i in range(n):
+            newPath = newIteration[i]
 
-        paths[i] = newPath
+            for neighbor in graph[i]:
+                newPath = min(newPath, newIteration[neighbor] + 1)
 
-    # print(graph)
-    # print(paths)
-    print(' '.join(str(path) for path in paths))
+            newIteration[i] = newPath
+
+        if newIteration == paths:
+            break
+
+        paths = newIteration
+
+    # wrong = 10
+    # print(graph[wrong])
+    # print(paths[wrong])
+    answer = ' '.join(str(path) for path in paths)
+    # print('#######')
+    print(answer)
+    # print(answer == EXPECTED_4)
 
 
 main()
